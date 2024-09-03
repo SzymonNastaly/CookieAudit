e86ee8d758142736b0ff557eeea2218d46016997
SzymonNastaly <szymonnastaly@gmail.com>
2024-09-03 23:11:40 +0200

    🚸 Add smooth scrolling

--- entrypoints/pageInteractor.js
+++ entrypoints/pageInteractor.js
@@ -54,8 +54,34 @@ function isTrivialLink(currentUrl, linkUrl) {
   return targetLocation.origin === currentLocation.origin && targetLocation.pathname === currentLocation.pathname;
 }
 
+function easeInOutCubic(t) {
+  return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+}
+
+function animateScroll(startTime, start, distance, duration) {
+  const currentTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+  const timeElapsed = currentTime - startTime;
+  const progress = Math.min(timeElapsed / duration, 1);
+
+  window.scrollTo(0, start + distance * easeInOutCubic(progress));
+
+  if (timeElapsed < duration) {
+    requestAnimationFrame(() => animateScroll(startTime, start, distance, duration));
+  }
+}
+
+function smoothScroll(distance, duration) {
+  const start = window.pageYOffset;
+  const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+  animateScroll(startTime, start, distance, duration);
+}
+
 export default defineUnlistedScript(async () => {
-  window.scrollBy({top: window.innerHeight, behavior: 'smooth'});
+  /**
+   * Note: the window.scrollBy({top: window.innerHeight, behavior: 'smooth'}); did not scroll smoothly, it just jumped.
+   * This was even the case for new Chrome versions.
+   */
+  smoothScroll(window.innerHeight, 1000);
 
   await delay(1000);
 
